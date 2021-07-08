b8d93887c7300d19676542123e252f7825c1fc68
galvizlaura69 <galvizlaura69@gmail.com>
2021-07-08 14:18:50 -0500

    modal

--- src/pages/Presentacion.js
+++ src/pages/Presentacion.js
@@ -10,6 +10,61 @@ export default function Presentacion() {
       </p>
       <p className="parrafo">·	Acumulativas: cuando un coplero toma una tonada que ya tiene incorporada al ser “hablante” de esas músicas, la llena de contenido (texto) siguiendo patrones métricos y estróficos ya aprendidos por tradición oral. Es decir, a elementos ya apropiados —podemos decir acumulados— se agregan nuevas acciones que permiten transitar por las formas particulares de repentismo. Esto sucede con claridad en las formas de la trova, del contrapunteo o de la piqueria
      </p>
+     <img className="imagenmin" src="https://i.pinimg.com/236x/6b/5f/17/6b5f17f45147940b9e3aad2c0dc1d3dd--german-shepherd-husky-shepherd-dogs.jpg" 
+     alt="Imagen de prueba">
+       </img>
+       <p className="parrafo">·	Acumulativas: cuando un coplero toma una tonada que ya tiene incorporada al ser “hablante” de esas músicas, la llena de contenido (texto) siguiendo patrones métricos y estróficos ya aprendidos por tradición oral. Es decir, a elementos ya apropiados —podemos decir acumulados— se agregan nuevas acciones que permiten transitar por las formas particulares de repentismo. Esto sucede con claridad en las formas de la trova, del contrapunteo o de la piqueria
+      </p>
+
+        <table className="tablamin">
+        <tr>
+          <th>Eje</th>
+          <th>Eje</th>
+          <th>Eje</th> 
+          </tr>
+
+          <tr>
+            <td>Eje</td>
+            <td>Eje</td>
+            <td>Eje</td>
+          </tr>
+
+          <tr>
+           <td>Eje</td>
+           <td>Eje</td>
+           <td>Eje</td>
+          </tr>
+        </table>
+
+        <iframe className="videomin"
+        src="https://www.youtube.com/embed/ZSHyR--4Pek" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+       </iframe>
+
+        <p className="parrafo">Este es el modal de prueba para el libro min 
+        <sup><a data-toggle="modal" data-target="#notapie1">1</a></sup>
+        </p>
+    
     </div>
   );
 }
+
+
+
+// inicio modales
+
+<div id="notapie1" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    
+    <div class="modal-content">
+      <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">1</h4> 
+      </div>
+      <div class="modal-body">
+        <p>
+        modal de prueba
+         </p>
+      </div>
+
+      </div>
+      </div>
+      </div>
