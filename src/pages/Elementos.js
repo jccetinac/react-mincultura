d6085b3d26997530aa64ba747184699b8c644a60
galvizlaura69 <galvizlaura69@gmail.com>
2021-07-19 13:17:37 -0500

    introyarreglos

--- src/pages/Elementos.js
+++ src/pages/Elementos.js
@@ -3,8 +3,11 @@ import React from 'react';
 export default function Elementos() {
   return (
     <div className="page card ">
-      <h1 className="tituloLargo">ELEMENTOS QUE CONFIGURAN LAS PEDAGOGÍAS DE LAS MÚSICAS  DE TRADICIÓN ORAL</h1>
+      <h1 className="titulo">ELEMENTOS QUE CONFIGURAN LAS PEDAGOGÍAS DE LAS MÚSICAS  DE TRADICIÓN ORAL</h1>
       
+      <p className="parrafo">
+      En este aparte se presentan los elementos considerados como característicos de las pedagogías de las músicas de tradición oral. A pesar de que se presentan de manera separada, las dimensiones y procesos están entrelazados en el quehacer musical y pedagógico. Por esta razón, posterior a la descripción de estos elementos, se presentarán las pautas pedagógicas en las que están contenidos de manera paralela. Se invita a que en la práctica las dimensiones, procesos y líneas no se aborden de manera separada en “clases” o en “horas de” cuerpo, o de escucha, por ejemplo. Por el contrario, se espera que puedan proponerse actividades en las que se escuche desde el cuerpo y del movimiento, en actividades integradas en las que se estimule la memoria desde la imitación, o la exploración desde la oralidad, entre muchas otras posibilidades. 
+      </p>
     </div>
   );
 }
--- src/pages/Introduccion.js
+++ src/pages/Introduccion.js
@@ -111,8 +111,7 @@ export default function Introduccion() {
           
       </table>
 
-
-      <h6>Tomado de: (Arango, 2009, pág. 244)</h6>
+     <h6>Tomado de: (Arango, 2009, pág. 244)</h6>
       <br>
       </br>
 
@@ -122,15 +121,97 @@ export default function Introduccion() {
       <p className="parrafo">
       A diferencia de los anteriores documentos, este lineamiento tiene como centro las <b>pedagogías de las músicas de tradición oral; </b> aunque encuentra asidero en las músicas mismas, <b>su énfasis son sus pedagogías</b>, es decir los modos en los que se apropian e intervienen las mismas, las formas de construcción de conocimiento musical. 
       </p>
-      <ul className="bg-dark">
-        <li>
-      	Tiene como base las lógicas de las músicas tradicionales organizadas en ejes, como se señala en el cuadro 1, así como algunos de los repertorios propuestos en las cartillas de músicas tradicionales. 
-        </li>
-      </ul>
+      
       <p className="parrafo">
         Este lineamiento de Pedagogías de las músicas de tradición oral, guarda algunas conexiones con los trabajos anteriores:
       </p>
-      
+
+      <ul>
+        <li>
+      	Tiene como base las lógicas de las músicas tradicionales organizadas en ejes, como se señala en el cuadro 1, así como algunos de los repertorios propuestos en las cartillas de músicas tradicionales. 
+        </li>
+        <li>
+      	Recoge algunas de las elaboraciones de los lineamientos de formación musical, como algunos ejes formativos, sentidos y pautas para los procesos pedagógicos. Puede relacionarse por ejemplo el eje auditivo de los lineamientos anteriores, con la escucha, propuesta en el presente documento. La exploración, la creación y la práctica están presentes en todos los lineamientos, incluido este.
+        </li>
+      </ul>
+
+      <p className="parrafo">
+        Debe señalarse que, en este lineamiento, los elementos señalados anteriormente se proponen desde la lógica de las músicas de tradición oral, por lo que se alerta al lector o lectora, que tendrán un sentido particular en este documento. Se busca así evidenciar la diversidad de propuestas de abordaje de la formación musical, manteniendo algunos elementos que se consideran fundamentales.
+      </p>
+
+      <p className="parrafo">
+       A pesar de que este lineamiento no es un documento de investigación sobre las pedagogías de las músicas de tradición oral, sí se propone como una entrada, como una provocación a su exploración y valoración, como acciones que se constituyen en parte fundamental de las músicas mismas. Por ello, para su realización, se partió de un proceso de identificación de diferentes formas de apropiación de las músicas de tradición oral de algunas regiones del país (capítulo 1), para posteriormente construir una propuesta pedagógica general para las Escuelas Municipales de Música. De esta manera, el lineamiento recoge los aportes de las pedagogías de las músicas de tradición oral, sus lógicas y contextos, con el fin de proponer pautas para la formación y la práctica musical de estas Escuelas y de aquellos centros de formación musical a los cuales pueda resultarles atractivo e interesante (capítulo 2).
+      </p>
+
+      <p className="parrafo">
+      En este sentido, el presente lineamiento <b>no busca orientar o reemplazar los procesos pedagógicos de las músicas tradicionales, sino nutrir otras prácticas pedagógicas</b> recurriendo a los saberes acumulados alrededor de estas músicas. Con este propósito, el lineamiento describe:
+      </p>
+
+      <ul>
+        <li>
+      	Dimensiones: La Oralidad, la Memoria y la Corporalidad hacen parte de los procesos de aprendizaje, apropiación y enseñanza de las músicas de tradición oral y se consideran un aporte fundamental a las pedagogías musicales.
+        </li>
+        <li>
+      	Procesos transversales: La Observación, la Escucha, la Imitación y la Exploración, son entendidos como procesos presentes en todo el proceso formativo que permiten a los docentes y estudiantes acercarse a las músicas de tradición oral desde las vías propias de las comunidades que practican y viven estas músicas. 
+        </li>
+        <li>
+        Líneas pedagógicas: integran las dimensiones y procesos básicos, sugiriendo un camino para el abordaje de estas músicas. 
+        </li>
+        <li>
+        Un anexo con repertorios musicales y de la tradición oral que sugieren algunos niveles de complejidad para el proceso formativo. 
+        </li>
+      </ul>
+
+      <p className="parrafo">
+      Finalmente, este lineamiento propone un camino para que, a través de la investigación en Escuelas Municipales de Música, se hagan acercamientos a las formas de aprendizaje, enseñanza, apropiación y recreación de las músicas de tradición oral de las diferentes regiones del país, que permitan nutrir la labor pedagógica desde la diversidad cultural y musical con la que contamos (capítulo 3). 
+      </p>
+
+      <p className="parrafo">
+        Las <b>orientaciones pedagógicas</b> aquí contenidas no son métodos o pasos que deban seguirse de manera estricta, sino pautas generales que pueden ser adaptadas a unas músicas específicas y a unos contextos particulares. Así, los docentes de las Escuelas Municipales de Música podrán contar con unas orientaciones de una forma de entender, descubrir y enseñar, que podrán aterrizar a unas músicas y una población en particular. 
+      </p>
+
+      <p className="parrafo">
+      En conjunto con este documento se espera resaltar el valor que tienen las pedagogías de las músicas de tradición oral en el desarrollo de la memoria, la capacidad creativa y el disfrute. En consonancia con María Eugenia Londoño, se entiende que la recuperación de las memorias musicales colectivas es parte fundamental de la reconstrucción del tejido social que ha sido deteriorado por la guerra en el país: 
+      </p>
+     <em >
+     Colombia, violentada y desgarrada por la inequidad y la guerra, reclama una recuperación crítica de la memoria musical colectiva, capaz de orientar y fortalecer la acción oficial en diversos escenarios, memoria recreadora orientada a redireccionar las prácticas académicas reconocidas e institucionalizadas durante décadas. (Londoño, 2009, pág. 58)
+     </em>
+
+     <p className="parrafo">
+     Se espera también dar impulso al diálogo entre las músicas académicas y las músicas de tradición oral, reconociendo y sistematizando algunos de los elementos fundamentales de esta última. Finalmente, espera que los docentes de las Escuelas Municipales de Música tengan un insumo que les permita diversificar y enriquecer su quehacer pedagógico y contribuyan al fortalecimiento de las músicas de tradición oral del país. 
+    </p>
+
+    <p className="parrafo">
+    La construcción de este documento fue posible gracias al trabajo conjunto de representantes de algunas de las músicas de tradición oral, así como de personas cuyas experiencias los han llevado a escudriñar a profundidad el valor pedagógico arraigado en diferentes comunidades del país. La conversación y la reflexión desde el hacer, establecieron la ruta a través de la cual este documento tomó forma. De esta manera, el presente lineamiento se hizo posible gracias al afecto, dedicación e interés de:
+    </p>
+
+    <p className="parrafo">
+    Ana Iris Castillo, cantadora tradicional, codirectora del Grupo Tizón de la ciudad de Pasto. Ganadora de la mejor canción inédita en el Festival de Música Petronio Álvarez 2018. Directora de la fundación Folclórica de Música Tumbos de Cualimán. Miembro del Grupo Gestor Regional del Plan Especial de Salvaguardia. Licenciada en Básica Primaria con énfasis Lengua Castellana y especialista en Pedagogía.
+    </p>
+
+    <p className="parrafo">
+    Cesar de Jesús Hernández Músico tradicional y cultor de los Montes de María. Músico tradicional de conjuntos gaiteros. Ex director de la Casa de la Cultura. Investigador sobre los focos de música de gaitas en los Montes de María. Fundador del grupo Aires de los Montes de María en 1992. Maestro del proyecto Semilleros de Iniciación Musical de la Escuela de Música Lucho Bermúdez. Licenciado en educación básica con énfasis en lengua castellana e inglés. Especialista en Pedagogía. Técnico Laboral en Música Instrumental de Bellas Artes y Humanidades.
+    </p>
+
+    <p className="parrafo">
+    Efraín Franco: Integrante del grupo Canto al pueblo-Armadillo (1977-1984) dedicado a la recopilación, estudio e interpretación de músicas tradicionales. Autor del Documento base de cartografía de músicas tradicionales del Eje Centro Oriente (2004) y coautor de “Viva quien toca” Cartilla de Iniciación Musical, Músicas Andinas del Centro-Oriente Colombiano (2008) con el Ministerio de Cultura. Director del Ensamble de Músicas Campesinas de la ASAB-Universidad Distrital Francisco José de Caldas desde 2008.  
+    </p>
+
+    <p className="parrafo">
+    Gustavo Córdoba –Hijuelapo- es pintor, músico folclórico y gestor cultural. Nació en el municipio de Campoalegre – Huila, fue director de la Casa de la Cultura de Campoalegre y Consejero departamental de cultura en los años 2.000 - 2004. Con coplas y canciones de su autoría, condensa el sentir del pueblo Campoalegruno y Huilense y promueve un movimiento de la música folclórica que representa las luchas y la visión del pueblo Huilense, que defiende su territorio, sus tradiciones culturales, la paz y la convivencia.
+    </p>
+
+    <p className="parrafo">
+    Jhon Moreno: Nació en San Luís de Palenque, Casanare. Músico de la Universidad de los Andes. Entre otras labores, ha acompañado al Ministerio de Cultura en la formación de investigadores en música llanera para el eje del “Territorio Sonoro del Joropo” y ha coordinado el Plan Especial de Salvaguardia de Carácter Urgente de Cantos de Trabajo de Llano y el Plan Departamental de Música del Meta.
+    </p>
+
+    <p className="parrafo"> 
+    Jorge Sossa: Músico, docente e investigador de las músicas populares y tradicionales. Director de la Escuela Musical Nueva Cultura. Miembro del Comité permanente del Movimiento de la Canción Infantil Latinoamericana y caribeña, MOCILyC Coordinador de la Red Colombiana de la Canción Infantil, RedCi, Realizador del programa radial Con alma de niños de UNRadio, emisora de la Universidad Nacional de Colombia.
+    </p>
+
+    <p className="parrafo">
+    Esperamos sea de interés y utilidad, pero, sobre todo, que permita que, desde la música, cultivemos el orgullo de las tradiciones que se han resistido al olvido y hacen parte esencial de lo que somos como colombianos.
+    </p>  
     
 
 
