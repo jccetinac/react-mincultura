4c3668504896a20727510664d8cdee2d732ac052
galvizlaura69 <galvizlaura69@gmail.com>
2021-08-18 21:35:54 -0500

    home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 export default function Home() {
   return (
-    <div className=" card ">
+    <div className="pageHome card ">
         <div className="portadaHome">
         <div className="fondorojohome"></div>
-        
+        <h1 className="tituloHome">LINEAMIENTO PEDAGOGIAS DE LAS MUSICAS DE TRADICIO ORAL</h1>
         </div>
     
     </div>
