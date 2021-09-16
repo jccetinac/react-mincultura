37b8960b83210045bd43359dea86509b876d7ee1
galvizlaura69 <galvizlaura69@gmail.com>
2021-09-16 07:32:43 -0500

    bandera

--- src/pages/creditos.js
+++ src/pages/creditos.js
@@ -22,9 +22,96 @@ export default function Creditos() {
           <span className="rojoC">Proyecto editorial - </span>Jaime H. Quevedo Urrea
       </p>
 
+      <p className="parrafoC1">
+          <span className="rojoC">Coordinación del proceso por parte del Ministerio de Cultura:</span>
+      </p>
+     <p className="parrafoC1">
+          Claudia Mejía 
+     </p>
+     <p className="parrafoC2">
+          Liliana Flechas 
+     </p>
+     <p className="parrafoC1">
+          <span className="rojoC">Coordinación del proceso y redacción del texto: </span>
+      </p>
+      <p className="parrafoC2">
+         Paola López
+     </p>
+     <p className="parrafoC1">
+          <span className="rojoC">Coautores:</span>
+      </p>
+      <p className="parrafoC1">
+        Ana Iris Castillo
+      </p>
+     <p className="parrafoC1">
+        Carlos Fernando Cardona
+     </p>
+     <p className="parrafoC1">
+        César de Jesús Hernández
+     </p>
+     <p className="parrafoC1">
+        Efraín Franco
+     </p>
+     <p className="parrafoC1">
+        Gustavo Córdoba -Hijuelapo-
+     </p>
+     <p className="parrafoC1">
+        Jhon Moreno
+     </p>
+     <p className="parrafoC1">
+        Jorge Sossa
+     </p>
+     <p className="parrafoC2">
+       Paola López
+     </p>
+     <p className="parrafoC1">
+          <span className="rojoC">Dirección de Artes</span>
+      </p>
+      <p className="parrafoC1">
+         Plan Nacional de Música para la Convivencia
+     </p>
+     <p className="parrafoC1">
+       Carrera 8 # 8A-31 Bogotá, D.C., Colombia
+     </p>
+     <p className="parrafoC1">
+       Teléfonos: (+1) 342 41 00 Línea gratuita: 018000 938081
+     </p>
+     <p className="parrafoC1">
+     plandemusica@mincultura.gov.co / www.mincultura.gov.co
+     </p>
+     <p className="parrafoC1">
+        Primera edición 2021
+     </p>
+     <p className="parrafoC1">
+        ISBN Versión Digital PDF: XXXXXXXXXXXX
+     </p>
+     <p className="parrafoC2">
+        ... otros
+     </p>
+     <p className="parrafoC1">
+          <span className="rojoC">EDITORIAL D’ARTAGNAN</span>
+      </p>
+      <p className="parrafoC2">
+        Diseño, diagramación e ilustración
+     </p>
+   
+
+
+
+
+
+
+
+
+
+
+
+
       <p className="parrafoC3">
           Material multimedia de distribución gratuita con fines didácticos y culturales. Queda estrictamente prohibida su reproducción total o parcial con ánimo de lucro, por cualquier sistema o método electrónico o impreso sin la autorización expresa para ello.
         </p>
+
+       
 
 
 
