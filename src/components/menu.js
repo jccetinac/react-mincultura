71c3a25cb7f742e1fbdb36d80de1253de127ebc4
oscetrcan <ofcetinac@unal.edu.co>
2021-11-18 09:40:20 -0500

    cambios color rojo a morado, shadow

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 
@@ -7,76 +7,76 @@ export default function Menu() {
   return (
     <div className="card p-4 menu">
       
-      <p><Link to="/creditos">Créditos</Link></p>
-     <p><Link to="/presentacion">Presentación</Link></p>
-     <p><Link to="/introduccion">Introducción</Link></p>
+      <p><NavLink activeClassName="linkselected" to="/creditos">Créditos</NavLink></p>
+     <p><NavLink activeClassName="linkselected" to="/presentacion">Presentación</NavLink></p>
+     <p><NavLink activeClassName="linkselected" to="/introduccion">Introducción</NavLink></p>
      <p>
-       <Link to="/Elementos">Elementos que configuran las pedagogías de las músicas de tradición oral</Link>
+       <NavLink activeClassName="linkselected" to="/Elementos">Elementos que configuran las pedagogías de las músicas de tradición oral</NavLink>
       </p>
 
-     <p><Link to="/1.dimensiones">1. Dimensiones</Link></p>
+     <p><NavLink activeClassName="linkselected" to="/1.dimensiones">1. Dimensiones</NavLink></p>
         <ul>
           <li className="subitems">
-          <Link to="/1.1.oralidad"> 1.1. Oralidad</Link>
+          <NavLink activeClassName="linkselected" to="/1.1.oralidad"> 1.1. Oralidad</NavLink>
           </li>
           <li className="subitems">
-          <Link to="/1.2.memoria"> 1.2. Memoria</Link>
+          <NavLink activeClassName="linkselected" to="/1.2.memoria"> 1.2. Memoria</NavLink>
           </li>
           <li className="subitems">
-          <Link to="/1.3.corporalidad"> 1.3. Corporalidad</Link>
+          <NavLink activeClassName="linkselected" to="/1.3.corporalidad"> 1.3. Corporalidad</NavLink>
           </li>
         </ul>
-        <p><Link to="/2.ProcesosTransversales"> 2. Procesos transversales</Link></p>
+        <p><NavLink activeClassName="linkselected" to="/2.ProcesosTransversales"> 2. Procesos transversales</NavLink></p>
         <ul>
           <li className="subitems">
-          <Link to="/2.1.laObservacion"> 2.1. La observación: “pele el ojo”</Link>
+          <NavLink activeClassName="linkselected" to="/2.1.laObservacion"> 2.1. La observación: “pele el ojo”</NavLink>
           </li>
           <li className="subitems">
-          <Link to="/2.2.laEscucha"> 2.2. La Escucha: “pare oreja”</Link>
+          <NavLink activeClassName="linkselected" to="/2.2.laEscucha"> 2.2. La Escucha: “pare oreja”</NavLink>
           </li>
           <li className="subitems">
-          <Link to="/2.3.laImitacion"> 2.3. La imitación</Link>
+          <NavLink activeClassName="linkselected" to="/2.3.laImitacion"> 2.3. La imitación</NavLink>
           </li>
           <li className="subitems">
-          <Link to="/2.4.laExploracion"> 2.4. La exploración</Link>
+          <NavLink activeClassName="linkselected" to="/2.4.laExploracion"> 2.4. La exploración</NavLink>
           </li>
           <li className="subitems">
-          <Link to="/2.5.laCreacion"> 2.5. La creación y la improvisación</Link>
+          <NavLink activeClassName="linkselected" to="/2.5.laCreacion"> 2.5. La creación y la improvisación</NavLink>
           </li>
         </ul>
-        <p><Link to="/3.LineasPedagogicas"> 3. Líneas pedagógicas</Link></p>
+        <p><NavLink activeClassName="linkselected" to="/3.LineasPedagogicas"> 3. Líneas pedagógicas</NavLink></p>
         <ul>
           <li className="subitems">
-          <Link to="/3.1.textoVoz"> 3.1. Texto, voz y canto</Link>
+          <NavLink activeClassName="linkselected" to="/3.1.textoVoz"> 3.1. Texto, voz y canto</NavLink>
           </li>
           <li className="subitems">
-          <Link to="/3.2.cuerpoYritmo">3.2. Cuerpo, ritmo y movimiento</Link>
+          <NavLink activeClassName="linkselected" to="/3.2.cuerpoYritmo">3.2. Cuerpo, ritmo y movimiento</NavLink>
           </li>
           <li className="subitems">
-          <Link to="/3.3.loArmonico">3.3. Lo armónico y melódico</Link>
+          <NavLink activeClassName="linkselected" to="/3.3.loArmonico">3.3. Lo armónico y melódico</NavLink>
           </li>
           <li className="subitems">
-          <Link to="/3.4.ensambleYmontaje">3.4. Ensamble y montaje</Link>
+          <NavLink activeClassName="linkselected" to="/3.4.ensambleYmontaje">3.4. Ensamble y montaje</NavLink>
           </li>
         </ul>
-        <p><Link to="/4.PautasDeInvestigacion">4. Pautas de Investigación de las pedagogías de las músicas de tradición oral</Link>
+        <p><NavLink activeClassName="linkselected" to="/4.PautasDeInvestigacion">4. Pautas de Investigación de las pedagogías de las músicas de tradición oral</NavLink>
            </p>
            <ul>
              <li className="subitems">
-             <Link to="/4.1.porqueInvestigar">4.1. ¿Por qué investigar las pedagogías de las músicas de tradición oral?</Link>
+             <NavLink activeClassName="linkselected" to="/4.1.porqueInvestigar">4.1. ¿Por qué investigar las pedagogías de las músicas de tradición oral?</NavLink>
              </li>
              <li className="subitems">
-             <Link to="/4.2.queInvestigar">4.2. ¿Qué investigar en las pedagogías de las músicas de tradición oral?</Link>
+             <NavLink activeClassName="linkselected" to="/4.2.queInvestigar">4.2. ¿Qué investigar en las pedagogías de las músicas de tradición oral?</NavLink>
              </li>
              <li className="subitems">
-             <Link to="/4.3.comoInvestigar">4.3. ¿Cómo investigar las pedagogías de las músicas de tradición oral desde la escuela?</Link>        
+             <NavLink activeClassName="linkselected" to="/4.3.comoInvestigar">4.3. ¿Cómo investigar las pedagogías de las músicas de tradición oral desde la escuela?</NavLink>        
              </li>
              <li className="subitems">
-             <Link to="/4.4.quienesPuedenInvestigar">4.4. ¿Quiénes pueden investigar las pedagogías de las músicas de tradición oral?</Link>       
+             <NavLink activeClassName="linkselected" to="/4.4.quienesPuedenInvestigar">4.4. ¿Quiénes pueden investigar las pedagogías de las músicas de tradición oral?</NavLink>       
               </li>
              </ul>
-           <p><Link to="/5.Anexos2">5. Anexos</Link></p>
-           <p><Link to="/6.Bibliografia">6. Bibliografía</Link></p>
+           <p><NavLink activeClassName="linkselected" to="/5.Anexos2">5. Anexos</NavLink></p>
+           <p><NavLink activeClassName="linkselected" to="/6.Bibliografia">6. Bibliografía</NavLink></p>
           
            
       </div>
