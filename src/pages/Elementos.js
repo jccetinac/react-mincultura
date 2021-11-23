f2a5ceb7ce47e840bace87d642cecd0f9de558f3
oscetrcan <ofcetinac@unal.edu.co>
2021-11-23 10:41:10 -0500

    ajuste grafico en elementos

--- src/pages/Elementos.js
+++ src/pages/Elementos.js
@@ -9,7 +9,7 @@ export default function Elementos() {
       En este aparte se presentan los elementos considerados como característicos de las pedagogías de las músicas de tradición oral. A pesar de que se presentan de manera separada, las dimensiones y procesos están entrelazados en el quehacer musical y pedagógico. Por esta razón, posterior a la descripción de estos elementos, se presentarán las pautas pedagógicas en las que están contenidos de manera paralela. Se invita a que en la práctica las dimensiones, procesos y líneas no se aborden de manera separada en “clases” o en “horas de” cuerpo, o de escucha, por ejemplo. Por el contrario, se espera que puedan proponerse actividades en las que se escuche desde el cuerpo y del movimiento, en actividades integradas en las que se estimule la memoria desde la imitación, o la exploración desde la oralidad, entre muchas otras posibilidades. 
       </p>
 
-      <img className="graficocontenido" src="https://firebasestorage.googleapis.com/v0/b/mincultura-lineamientos.appspot.com/o/grafico1.png?alt=media&token=907b33c0-9938-436c-b248-a412e85c8f30">
+      <img className="graficocontenido" src="https://firebasestorage.googleapis.com/v0/b/mincultura-lineamientos.appspot.com/o/grafico1_EDITADO.png?alt=media&token=2df209e0-e8b7-4e9f-861c-539a8b4e28a0">
        </img>
 
     </div>
